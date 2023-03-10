11bb4676bd3b73254dba6f9ce32e629a131ef3ad
David Cheadle <a@b.c>
2023-03-10 19:40:32 +0000

    Added the Arduino command list
    - Added the list of commands used by the Arduino for the robot interface.

---
+++ Arduino/cmd_list.h
@@ -0,0 +1,11 @@
+#ifndef CMD_LIST_H
+#define CMD_LIST_H
+
+const char CMD_LEFT = "L"
+const char CMD_RIGHT = "R"
+const char CMD_CENTRE = "C"
+const char CMD_FORWARD = "F"
+const char CMD_BACKWARD = "B"
+const char CMD_STOP = "S"
+
+#endif /* CMD_LIST_H */ 
